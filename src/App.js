88df37d7b91e667d7c95cd993f20bb1fa8c9109e
Arfa <iramtechy513@gmail.com>
2023-03-08 17:23:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import React,{useState} from 'react'
-import './components/css/Style.css';
-import './App.css';
-import axios from "axios";
-import CitySearch from './components/CitySearch';
-import WeatherCard from './components/WeatherCard';
+import React, { useState, useEffect } from "react";
+import Header from './components/Header';
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 
 function App() {
-  const [weatherResult,setweatherResult]=useState(null)
+  const { v4: uuidv4 } = require('uuid');
+  const LOCAL_STORAGE_KEY = 'Contacts';
+  const [contacts, setcontacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setcontacts([...contacts, { id: uuidv4(), ...contact }]);
+    console.log(contact.id)
+    //The spread operator can be used to take an existing array and add another element to it while still preserving the original array (famous original array’s?).
+  }
+
+
+  const removeContactHandler = (id) => {
+    //filter returns all the elements of arr satisfying condition
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    })
+    setcontacts(newContactList);
+  }
+
   
-  const onSearchSubmit = async (searchInputValue) => {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=ca261c971d5638db9d4d6cbccc1f093d`)
-    console.log(response.data)
-    setweatherResult(response.data)
-    console.log(weatherResult)
- }
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }
+    , [contacts]);
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (retrieveContacts)
+      setcontacts(retrieveContacts);
+  }
+    , []);
+
   return (
-   
-     <div className="container my-5">
-                <h1 className="text-center title">Weather in</h1>
-                 <CitySearch onSearchSubmit = {onSearchSubmit} />
-                 {weatherResult ?  <WeatherCard weatherResult = {weatherResult} /> : <div></div>}
-            </div>
-    
+    <div className="App">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <h3 
+              style={{marginTop:'20px',fontFamily:'sans-serif',textDecorationLine:'underline'}}>CONTACT LIST</h3>
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
   )
 }
-
-export default App
+export default App;
